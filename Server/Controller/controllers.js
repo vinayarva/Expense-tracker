f21c08e73c281cff5ebbfc4d4e46cfedb401beed
vinayarva <160695351+vinayarva@users.noreply.github.com>
2024-10-14 19:31:26 +0530

    password encription
    added the password hashing

--- Server/Controller/controllers.js
+++ Server/Controller/controllers.js
@@ -1,13 +1,33 @@
-const { where } = require("sequelize");
+const bcrypt = require("bcrypt");
+
 const User = require("../models/user_model");
 
 module.exports.userSignUp = (req, res, next) => {
-  User.create(req.body)
+  let userDetails = req.body;
+
+  async function userSignIn() {
+    try {
+      const hash = await bcrypt.hash(userDetails.password, 10);
+
+      userDetails["password"] = hash;
+
+      return userDetails;
+    } catch {
+      console.log(err);
+    }
+  }
+  userSignIn()
     .then((result) => {
-      res.send("user account is created");
+      User.create(result)
+        .then((result) => {
+          res.send("user account is created");
+        })
+        .catch((err) => {
+          res.status(401).send("User already have an account");
+        });
     })
     .catch((err) => {
-      res.send(true);
+      console.log(err);
     });
 };
 
@@ -15,17 +35,23 @@ module.exports.userLogin = (req, res, next) => {
   User.findOne({ where: { email: req.body.email } })
     .then((result) => {
       if (result) {
-        if (result.password === req.body.password) {
-          res.send("Login Successful");
-        } else {
-          res.send("Password is wrong, Please Enter the Right Password");
-        }
+        bcrypt.compare(
+          req.body.password,
+          result.password,
+          function (err, data) {
+            if (data) {
+              res.send("Login Successful");
+            } else {
+              res.send("Password is wrong, Please Enter the Right Password");
+            }
+          }
+        );
       } else {
         res.send("Account not found, please create an account");
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(404).send("An error occurred during login");
+      res.status(401).send("An error occurred during login");
     });
 };
